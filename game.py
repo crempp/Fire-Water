fbaf92183e3920e78c6fd68cf3f474bd247bd473
Chad Rempp <crempp@gmail.com>
2012-01-12 22:49:29 -0600

    Some initial tests

---
+++ game.py
@@ -0,0 +1,31 @@
+# Author: Ryan Myers
+# Models: Jeff Styers, Reagan Heller
+
+
+# Last Updated: 6/13/2005
+#
+# This tutorial provides an example of creating a character
+# and having it walk around on uneven terrain, as well
+# as implementing a fully rotatable camera.
+
+import direct.directbase.DirectStart
+from direct.showbase.DirectObject import DirectObject
+#from panda3d.core import CollisionTraverser,CollisionNode
+#from panda3d.core import CollisionHandlerQueue,CollisionRay
+#from panda3d.core import Filename,AmbientLight,DirectionalLight
+#from panda3d.core import PandaNode,NodePath,Camera,TextNode
+#from panda3d.core import Vec3,Vec4,BitMask32
+#from direct.gui.OnscreenText import OnscreenText
+#from direct.actor.Actor import Actor
+
+#import random, sys, os, math
+
+class World(DirectObject):
+
+    def __init__(self):
+        pass
+        
+        
+
+w = World()
+run()
