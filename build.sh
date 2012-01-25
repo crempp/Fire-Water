a5700bdae2131f95406d4ea5778c260b4408b02c
Chad Rempp <crempp@gmail.com>
2012-01-24 21:38:01 -0600

    Added missing files from previous commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+panda3d ../ppackage.p3d -i . firewater.pdef
---
+++ main.py
@@ -0,0 +1,131 @@
+import direct.directbase.DirectStart
+from direct.showbase.DirectObject import DirectObject
+#from panda3d.core import CollisionTraverser,CollisionNode
+#from panda3d.core import CollisionHandlerQueue,CollisionRay
+#from panda3d.core import Filename,AmbientLight,DirectionalLight
+#from panda3d.core import PandaNode,NodePath,Camera,TextNode
+#from panda3d.core import Vec3,Vec4,BitMask32
+#from direct.gui.OnscreenText import OnscreenText
+#from direct.actor.Actor import Actor
+from pandac.PandaModules import NodePath
+from pandac.PandaModules import Vec3
+
+from direct.gui.OnscreenText import OnscreenText
+from pandac.PandaModules import PandaNode,Camera,TextNode
+from direct.gui.DirectGui import *
+from direct.interval.IntervalGlobal import *
+
+#import random, sys, os, math
+
+from Water import Water
+from Representations import BattleShip
+
+import Controller
+import Event
+
+from Log import LogConsole
+from CameraMgr import CameraManager
+
+class World(DirectObject):
+    hitRadius = 3
+    
+    def __init__(self):
+        # Setup logger
+        import __builtin__
+        __builtin__.LOG = LogConsole()
+        
+        # Initialize event dispatcher
+        self._dispatcher = Event.Dispatcher()        
+        
+        # Setup controls
+        self.keyboardcontroller = Controller.KeyboardController()
+        self.mousecontroller = Controller.MouseController()
+        
+        # Build game board
+        __builtin__.boardNP = NodePath("Game Board Parent")
+        boardNP.reparentTo(render)
+        
+        self.gamePieces = []
+        
+        ########################################################################
+        w = Water(boardNP)
+        
+        s1 = BattleShip(parent=w.baseNode, pos=Vec3(50, 0, 0))
+        s2 = BattleShip(parent=w.baseNode, pos=Vec3(-30, 10, 0))
+        
+        s1.setGame(self)
+        s2.setGame(self)
+        
+        s1.activate()
+        
+        self.gamePieces = [s1, s2]
+        
+        ########################################################################        
+        
+        # Camera
+        c = CameraManager()
+        c.startCamera()
+        c.setTarget(s1)
+        
+        # Debugging
+        self.coordDebug()
+        #print(s1.pos)
+        #print(s1.model.getPos())
+        
+    def checkHit(self, pos):
+        for s in self.gamePieces:
+            hitVec = pos - s.pos
+            dist   = hitVec.length()
+            
+            if (dist < self.hitRadius):
+                LOG.debug("Hit!!!!!!!!!!!!")
+                hitSequence=Sequence(s.explode(), Func(s.model.removeNode))
+                hitSequence.start()
+            
+            LOG.debug("hit  = %s"%pos)
+            LOG.debug("ship = %s"%s.pos)
+            LOG.debug("dist = %s"%dist)
+    
+    # Coord debug functions
+    # From http://code.google.com/p/python-panda3d-examples/wiki/PandaCoords
+    def coordDebug(self):
+        for i in range(0,51):
+            self.printText("X", "|", (1,0,0)).setPos(i,0,0) 
+        
+        for i in range(0,51):
+            self.printText("Y", "|", (0,1,0)).setPos(0,i,0)  
+                
+        for i in range(0,51):
+            self.printText("Z", "-", (0,0,1)).setPos(0,0,i) 
+        
+        self.printText("XL", "X", (0,0,0)).setPos(11.5,0,0) 
+        self.printText("YL", "Y", (0,0,0)).setPos(1,10,0) 
+        self.printText("YL", "Z", (0,0,0)).setPos(1,0,10) 
+        self.printText("OL", "@", (0,0,0)).setPos(0,0,0) 
+        
+        OnscreenText(text = '(0 , 0)', pos = (.1, .05), scale = 0, fg=(1,1,1,1))
+        for i in range(-20,20):
+            OnscreenText(text = '.', pos = (0, i/float(10)), scale = 0, fg=(1,1,1,1))
+        for i in range(-20,20):
+            OnscreenText(text = '.', pos = (i/float(10), 0), scale = 0, fg=(1,1,1,1))
+            
+        OnscreenText(text = 'X', pos = (1.3, .1), scale = 0, fg=(1,1,1,1))
+        OnscreenText(text = 'Y', pos = (.1, .9), scale = 0, fg=(1,1,1,1))
+        
+        
+    def printText(self, name, message, color): 
+        text = TextNode(name) # create a TextNode. Note that 'name' is not the text, rather it is a name that identifies the object.
+        text.setText(message) # Here we set the text of the TextNode
+        x,y,z = color # break apart the color tuple
+        text.setTextColor(x,y,z, 1) # Set the text color from the color tuple
+    
+        text3d = NodePath(text) # Here we create a NodePath from the TextNode, so that we can manipulate it 'in world'
+        text3d.reparentTo(render) # This is important. We reparent the text NodePath to the 'render' tree. 
+        # The render tree contains what is going to be rendered into the 3D WORLD. 
+        # If we reparented it to 'render2d' instead, it would appear in the 2D WORLD. 
+        # The 2D WORLD, from the documentation, is as if someone had written on your computer screen.
+        return text3d # return the NodePath for further use
+        
+        
+w = World()
+run()
